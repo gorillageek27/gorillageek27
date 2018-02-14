c312f49b9392daf641feb9faec92f041495acf85
gorillageek27 <35504088+gorillageek27@users.noreply.github.com>
2018-02-14 13:01:17 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
-#include <stdio.h>
 using namespace std;
 int main() {
-	double bill;
-	bool doexit = false;
-	
-	while(!doexit) 
-	{cout << "please enter a bill for food you order at a resturaunt" << endl;
-	cin >>bill;
-		if (bill == 0)
-			doexit = true;
-		bill = bill * .15;
-		cout << bill;
+	char input;
 
-
-
+	cout << "Press Y" << endl;
+	cin >> input;
+	while (input == 'y') {
+		cout << "<3" << endl;
+		system("pause");
 	}
 }

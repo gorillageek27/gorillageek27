dc15c7c7510bff9ac80abe92efec6ce12f61491d
gorillageek27 <35504088+gorillageek27@users.noreply.github.com>
2018-02-21 13:49:29 -0700

    Add files via upload

---
+++ beep beep.cpp
@@ -0,0 +1,26 @@
+#include <Windows.h>
+using namespace std;
+#include <iostream>
+
+int main() {
+	int a;
+	cout <<"would you like to hear a song?" << endl;
+	cin >> a;
+	for (int i = 0; i < a; i++) {
+
+		Beep(400, 200);
+
+
+	}
+		
+
+
+
+	
+
+
+
+
+
+
+}

916f264d0394bd8ae881d408bd7fc911e9b9e5a0
gorillageek27 <35504088+gorillageek27@users.noreply.github.com>
2018-03-19 12:52:26 -0600

    Add files via upload

---
+++ messagebox.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+	while(1)
+		MessageBox(NULL, ("hahahahah oh my god you suck"), ("EAT IT"), MB_OK | MB_SYSTEMMODAL);
+	
+}

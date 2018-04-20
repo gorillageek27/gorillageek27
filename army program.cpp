27ab5c3704b97746f2976c57b47db4d595a38076
gorillageek27 <35504088+gorillageek27@users.noreply.github.com>
2018-04-20 12:48:04 -0600

    Add files via upload

---
+++ army program.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+	
+	char input;
+	char a;
+	char b;
+	char c;
+	char d;
+	cout << "please enter a letter A - d" << endl << endl;
+	cin >> input;
+	
+	if (input == 'a')
+		cout << "alpha" << endl << endl;
+
+	else if (input == 'b')
+		cout << "bravo" << endl << endl;
+	else if (input == 'c')
+		cout << "charlie" << endl << endl;
+	else if (input == 'd')
+		cout << "delta" << endl << endl;
+
+		}
+	
